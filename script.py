50b88240776638abee9a79c1671feae2273b4b34
xpartla <xpartla@stuba.sk>
2025-03-23 20:39:43 +0100

    using numpy arrays, better arg handling, predefined matrices, performance updates

--- script.py
+++ script.py
@@ -1,47 +1,38 @@
 from datetime import datetime
 import numpy as np
-import pandas as pd
-import sys
-
+import argparse
+from functools import lru_cache
 
 def parse_arguments():
-    if len(sys.argv) < 7 or len(sys.argv) > 8:
-        print("Usage: script.py -i1 <input_file_1> -2 <input_file_2> -s <score_file> (-p {protein} | -d {DNA} )")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Better emboss water :) ")
 
-    args = {}
-    optional_flag = None
+    parser.add_argument("-p", action="store_true", help="Use protein scoring matrix (blossum62.txt)")
+    parser.add_argument("-d", action="store_true", help="Use DNA scoring matrix (dna.txt)")
+    parser.add_argument("-s", metavar="scoreFile", type=str, help="Specify a custom scoring matrix file")
+    parser.add_argument("-r", action="store_true", help="use recursive algorithm instead of smith-waterman")
 
-    i = 1
-    while i < len(sys.argv):
-        if sys.argv[i] == '-i1':
-            args['firstInput'] = sys.argv[i + 1]
-            i += 2
-        elif sys.argv[i] == '-i2':
-            args['secondInput'] = sys.argv[i + 1]
-            i += 2
-        elif sys.argv[i] == '-s':
-            args['score'] = sys.argv[i + 1]
-            i += 2
-        elif sys.argv[i] in ('-p', '-d'):
-            if optional_flag is not None:
-                print("Error: Only one of -p or -d can be specified.")
-                sys.exit(1)
-            optional_flag = sys.argv[i]
-            i += 1
-        else:
-            print(f"Unknown argument: {sys.argv[i]}")
-            sys.exit(1)
+    parser.add_argument("input_files", nargs=2, help="Two FASTA input files with sequences")
 
-    if 'firstInput' not in args or 'secondInput' not in args or 'score' not in args:
-        print("Error: Missing required arguments -i and -s")
-        sys.exit(1)
+    args = parser.parse_args()
 
-    if optional_flag is None:
-        print("Error: One of -p or -d must be specified")
-        sys.exit(1)
+    selected_score_options = [args.p, args.d, args.s is not None]
+    if sum(selected_score_options) > 1:
+        parser.error("You can use only one -p, -d, or -s <file>.")
 
-    return args['firstInput'], args['secondInput'], args['score'], optional_flag
+    if args.p:
+        score_file = "blosum62.txt"
+    elif args.d:
+        score_file = "dna.txt"
+    elif args.s:
+        score_file = args.s
+    else:
+        score_file = ''
+        parser.error("You must specify a scoring matrix using -p, -d, or -s <file>.")
+
+    first_input, second_input = args.input_files
+
+    return first_input, second_input, score_file, args.r
+
 
 
 def readInput(input):
@@ -54,7 +45,6 @@ def readInput(input):
                 continue
             sequence.append(line.strip())
     return ''.join(sequence)
-
 
 def readMatrix(score):
     with open(score) as f:
@@ -70,30 +60,33 @@ def readMatrix(score):
         scores[row] = dict(zip(cols, row_scores))
     return scores
 
-
 def outputScores(seq1, seq2, scores, gap_open=10, gap_extend=0.5):
     STOP, LEFT, UP, DIAGONAL = 0, 1, 2, 3
 
-    score_matrix = pd.DataFrame(0.0, index=range(len(seq2) + 1), columns=range(len(seq1) + 1))
-    gap_x = pd.DataFrame(0.0, index=range(len(seq2) + 1), columns=range(len(seq1) + 1))
-    gap_y = pd.DataFrame(0.0, index=range(len(seq2) + 1), columns=range(len(seq1) + 1))
-    traceback_matrix = pd.DataFrame(0.0, index=range(len(seq2) + 1), columns=range(len(seq1) + 1))
+    score_matrix = np.zeros((len(seq2) + 1, len(seq1) + 1))
+    gap_x = np.zeros((len(seq2) + 1, len(seq1) + 1))
+    gap_y = np.zeros((len(seq2) + 1, len(seq1) + 1))
+    traceback_matrix = np.zeros((len(seq2) + 1, len(seq1) + 1), dtype=int)
 
     for i in range(1, len(seq1) + 1):
         for j in range(1, len(seq2) + 1):
-            gap_x.iloc[j, i] = max(-gap_open + score_matrix.iloc[j, i - 1], -gap_extend + gap_x.iloc[j, i - 1])
-            gap_y.iloc[j, i] = max(-gap_open + score_matrix.iloc[j - 1, i], gap_extend + gap_y.iloc[j - 1, i])
-            score_matrix.iloc[j, i] = max(scores[seq1[i - 1]][seq2[j - 1]] + score_matrix.iloc[j - 1, i - 1],
-                                          gap_x.iloc[j, i], gap_y.iloc[j, i], 0)
+            gap_x[j, i] = np.maximum(-gap_open + score_matrix[j, i - 1], -gap_extend + gap_x[j, i - 1])
+            gap_y[j, i] = np.maximum(-gap_open + score_matrix[j - 1, i], gap_extend + gap_y[j - 1, i])
+            score_matrix[j, i] = np.max([
+                scores[seq1[i - 1]][seq2[j - 1]] + score_matrix[j - 1, i - 1],
+                gap_x[j, i],
+                gap_y[j, i],
+                0
+            ])
 
-            if score_matrix.iloc[j, i] == 0:
-                traceback_matrix.iloc[j, i] = STOP
-            elif score_matrix.iloc[j, i] == gap_y.iloc[j, i]:
-                traceback_matrix.iloc[j, i] = LEFT
-            elif score_matrix.iloc[j, i] == gap_x.iloc[j, i]:
-                traceback_matrix.iloc[j, i] = UP
-            elif score_matrix.iloc[j, i] == scores[seq1[i - 1]][seq2[j - 1]] + score_matrix.iloc[j - 1, i - 1]:
-                traceback_matrix.iloc[j, i] = DIAGONAL
+            if score_matrix[j, i] == 0:
+                traceback_matrix[j, i] = STOP
+            elif score_matrix[j, i] == gap_y[j, i]:
+                traceback_matrix[j, i] = LEFT
+            elif score_matrix[j, i] == gap_x[j, i]:
+                traceback_matrix[j, i] = UP
+            elif score_matrix[j, i] == scores[seq1[i - 1]][seq2[j - 1]] + score_matrix[j - 1, i - 1]:
+                traceback_matrix[j, i] = DIAGONAL
 
     return gap_x, gap_y, score_matrix, traceback_matrix
 
@@ -101,26 +94,26 @@ def outputScores(seq1, seq2, scores, gap_open=10, gap_extend=0.5):
 def traceback(score_matrix, traceback_matrix, seq1, seq2, STOP=0, LEFT=1, UP=2, DIAGONAL=3):
     aligned_seq1, aligned_seq2, match = [], [], []
 
-    max_value = score_matrix.to_numpy().max()
-    max_j, max_i = np.where(score_matrix.to_numpy() == max_value)
+    max_value = score_matrix.max()
+    max_j, max_i = np.where(score_matrix == max_value)
     max_j, max_i = int(max_j[0]), int(max_i[0])
 
     post1 = seq1[max_i:] if max_i < len(seq1) else ''
     post2 = seq2[max_j:] if max_j < len(seq2) else ''
 
-    while traceback_matrix.iloc[max_j, max_i] != STOP:
-        if traceback_matrix.iloc[max_j, max_i] == DIAGONAL:
+    while traceback_matrix[max_j, max_i] != STOP:
+        if traceback_matrix[max_j, max_i] == DIAGONAL:
             aligned_seq1.append(seq1[max_i - 1])
             aligned_seq2.append(seq2[max_j - 1])
             match.append("|" if seq1[max_i - 1] == seq2[max_j - 1] else ".")
             max_i -= 1
             max_j -= 1
-        elif traceback_matrix.iloc[max_j, max_i] == UP:
+        elif traceback_matrix[max_j, max_i] == UP:
             aligned_seq1.append(seq1[max_i - 1])
             aligned_seq2.append('-')
             match.append(" ")
             max_i -= 1
-        elif traceback_matrix.iloc[max_j, max_i] == LEFT:
+        elif traceback_matrix[max_j, max_i] == LEFT:
             aligned_seq1.append('-')
             aligned_seq2.append(seq2[max_j - 1])
             match.append(" ")
@@ -140,40 +133,39 @@ def traceback(score_matrix, traceback_matrix, seq1, seq2, STOP=0, LEFT=1, UP=2, 
 
     return aligned_seq1, aligned_seq2, match
 
-
-def outputFile(aligned_seq1, aligned_seq2, match, score_matrix, seq1, seq2):
+def outputFile(aligned_seq1, aligned_seq2, match, score_matrix, seq1, seq2, runTime, recursive=False):
     with open('output.txt', 'w') as f:
         f.write('#' * 50 + '\n')
         f.write('Rundate: ' + datetime.now().isoformat(timespec='seconds') + '\n')
+        f.write('Execution time: ' + str(runTime) + '\n')
         f.write('# seq_1 :' + seq1 + '\n# seq_2: ' + seq2 + '\n')
         f.write('# gapOpen 10 \n')
         f.write('# gapExtend 0.5 \n')
         f.write('#' * 50 + '\n\n')
 
         f.write('#=================================================\n')
-        f.write('# Alignment Score: ' + str(max(score_matrix.max())) + '\n')
+        f.write('# Alignment Score: ' + str(np.max(score_matrix)) + '\n')
         f.write('# Length: ' + str(len(match) - 2) + '\n')
         f.write('#=================================================\n\n')
         f.write(aligned_seq1 + '\n' + match + '\n' + aligned_seq2 + '\n')
         f.write('\n#-------------------------------------------------\n')
         f.write('#-------------------------------------------------\n')
+        if recursive:
+            f.write('recursion not yet implemented')
 
 
-def run(firstInput, secondInput, scoreFile, mode):
-    if mode not in ('-p', '-d'):
-        return
-
+def run(firstInput, secondInput, scoreFile, recursive):
     seq1 = readInput(firstInput)
     seq2 = readInput(secondInput)
 
     scores = readMatrix(scoreFile)
-
+    startTime = datetime.now()
     gap_x, gap_y, score_matrix, traceback_matrix = outputScores(seq1, seq2, scores)
 
     aligned_seq1, aligned_seq2, match = traceback(score_matrix, traceback_matrix, seq1, seq2)
+    endTime = datetime.now()
+    runTime = endTime - startTime
+    outputFile(aligned_seq1, aligned_seq2, match, score_matrix, seq1, seq2, runTime, recursive)
 
-    outputFile(aligned_seq1, aligned_seq2, match, score_matrix, seq1, seq2)
-
-
-firstInput, secondInput, scoreFile, mode = parse_arguments()
-run(firstInput, secondInput, scoreFile, mode)
+firstInput, secondInput, scoreFile, recursive = parse_arguments()
+run(firstInput, secondInput, scoreFile, recursive)
